4b58367fcc6600ce66c74f1db9a2a4bdd8fc7cb8
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-07-26 23:48:22 -0300

    Sets content description for screen readers

--- app/src/main/java/com/sam_chordas/android/stockhawk/rest/QuoteCursorAdapter.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/rest/QuoteCursorAdapter.java
@@ -1,5 +1,6 @@
 package com.sam_chordas.android.stockhawk.rest;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Color;
@@ -43,10 +44,14 @@ public class QuoteCursorAdapter extends CursorRecyclerViewAdapter<QuoteCursorAda
     return vh;
   }
 
+  @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
   @Override
   public void onBindViewHolder(final ViewHolder viewHolder, final Cursor cursor){
     viewHolder.symbol.setText(cursor.getString(cursor.getColumnIndex("symbol")));
+    viewHolder.symbol.setContentDescription(mContext.getString(R.string.a11y_symbol ,cursor.getString(cursor.getColumnIndex("symbol"))));
     viewHolder.bidPrice.setText(cursor.getString(cursor.getColumnIndex("bid_price")));
+    viewHolder.bidPrice.setContentDescription(mContext.getString(R.string.a11y_bid_price ,cursor.getString(cursor.getColumnIndex("bid_price"))));
+
     int sdk = Build.VERSION.SDK_INT;
     if (cursor.getInt(cursor.getColumnIndex("is_up")) == 1){
       if (sdk < Build.VERSION_CODES.JELLY_BEAN){
@@ -67,8 +72,11 @@ public class QuoteCursorAdapter extends CursorRecyclerViewAdapter<QuoteCursorAda
     }
     if (Utils.showPercent){
       viewHolder.change.setText(cursor.getString(cursor.getColumnIndex("percent_change")));
+      viewHolder.change.setContentDescription(mContext.getString(R.string.a11y_percent_change ,cursor.getString(cursor.getColumnIndex("percent_change"))));
     } else{
       viewHolder.change.setText(cursor.getString(cursor.getColumnIndex("change")));
+      viewHolder.change.setContentDescription(mContext.getString(R.string.a11y_change ,cursor.getString(cursor.getColumnIndex("change"))));
+
     }
   }
 
