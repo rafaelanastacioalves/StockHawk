2b3447e549760f96f964977af5056e6355ac4581
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-08-05 15:41:56 -0300

    Inserts a WilliamChart Example

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockHistoryActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockHistoryActivity.java
@@ -4,12 +4,26 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.sam_chordas.android.stockhawk.R;
+
+
 public class StockHistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_history);
+
+        // TODO Add rescuiing savedInstance
+        if (savedInstanceState == null ){
+            Bundle arguments = new Bundle();
+
+            StockHistoryActivityFragment fragment =  new StockHistoryActivityFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.stock_history_fragment_container,  fragment)
+                    .commit();
+
+
+        }
     }
 
 }
--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockHistoryActivityFragment.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockHistoryActivityFragment.java
@@ -1,11 +1,14 @@
 package com.sam_chordas.android.stockhawk.ui;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.db.chart.model.LineSet;
+import com.db.chart.view.LineChartView;
 import com.sam_chordas.android.stockhawk.R;
 
 
@@ -14,12 +17,29 @@ import com.sam_chordas.android.stockhawk.R;
  */
 public class StockHistoryActivityFragment extends Fragment {
 
+    private final String LOG_TAG = getClass().getSimpleName();
+    private String[] labels = {"p1", "p2", "p3" };
+    private float[] values = {(float) 1.2, (float) 2.0, (float) 3.0};
+
     public StockHistoryActivityFragment() {
+        super();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_stock_history, container, false);
-    }
+        if (savedInstanceState != null && savedInstanceState.containsKey("SYMBOL_EXTRA")){
+            Log.i(LOG_TAG,"Has Extra!");
+        }
+        View viewRoot = inflater.inflate(R.layout.fragment_stock_history, container, false);
+
+        LineChartView lChart = (LineChartView) viewRoot.findViewById(R.id.linechart);
+
+        LineSet ls = new LineSet(labels, values);
+        lChart.addData(ls);
+        Log.i(LOG_TAG,"Showing chart Data!");
+
+        lChart.show();
+        return viewRoot;
+        }
 }
