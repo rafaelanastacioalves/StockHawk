2b77bc2dc3abbc17a36b93248533fb4adcc27e16
Rafael Anastacio Alves <rafaelanastacioalves@gmail.com>
2016-08-10 16:30:43 -0300

    Add classes for widget

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StocksWidgetProvider.java
@@ -0,0 +1,9 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by rafaelanastacioalves on 8/10/16.
+ */
+public class StocksWidgetProvider extends AppWidgetProvider {
+}
---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/WidgetStocksRemoteViewsService.java
@@ -0,0 +1,14 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+/**
+ * Created by rafaelanastacioalves on 8/10/16.
+ */
+public class WidgetStocksRemoteViewsService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return null;
+    }
+}
